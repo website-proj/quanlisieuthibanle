15541370e832c6e36b1ba4c7b6c9fc46605939f9
nguyencongmanh2004 <manh20102004@gmail.com>
2024-12-07 22:07:46 +0700

    router component

---
+++ app/Routers/api_router.py
@@ -0,0 +1,11 @@
+from fastapi import APIRouter
+
+from app.Routers import admin_user , admin_category , admin_product , categories ,products ,users
+
+router = APIRouter()
+
+router.include_router(admin_user.router, tags=["admin/user"], prefix="/admin")
+router.include_router(admin_product.router, tags=["admin/product"], prefix="/admin")
+router.include_router(admin_category.router, tags=["admin/category"], prefix="/admin")
+router.include_router(products.router, tags=["Product"], prefix="/product")
+router.include_router(users.router, tags=["users"], prefix="/users")
