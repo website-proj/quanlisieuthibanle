d37bd39dff4c9adb2cd7a2efaaefd5e19809e19e
nguyencongmanh2004 <manh20102004@gmail.com>
2024-12-07 22:11:52 +0700

    initial commit

--- app/Routers/users.py
+++ app/Routers/users.py
@@ -1,59 +1,59 @@
 from fastapi import Depends, APIRouter, FastAPI, HTTPException
+from fastapi.security import OAuth2PasswordRequestForm
 from pydantic import BaseModel
-from sqlalchemy.sql.functions import current_user
+
 
 from app.core.security import create_access_token
 from app.model.users import User
-from app.schemas.schema_base import DataResponse
+# from app.schemas.schema_base import DataResponse
 from app.schemas.schema_token import Token
-from app.schemas.schema_user import UserCreateRequest, UserOut, UserRegisterRequest , login , userUpdateRequest
+from app.schemas.schema_user import UserCreateRequest, UserOut, UserRegisterRequest, login, userUpdateRequest, \
+    passwordChangeRequest
+from app.services.auth import AuthService
 from app.services.users import UserService
 from app.helper.login_manager import login_required
 from sqlalchemy.orm import Session
 from app.utils.responses import ResponseHandler
 from app.db.base import get_db
 
-router = APIRouter(tags=["users"] , prefix="/users")
+router = APIRouter()
 
-# #create newUser
-# @router.post("/", response_model= UserOut )
-# async def create_user(data : UserCreateRequest , user_service: UserService = Depends() ) :
-#     """
-#     API Create New User
-#     """
-#     try :
-#         new_user = user_service.create_user()
+
 @router.post("/")
 async def register_user(user : UserRegisterRequest  , user_service : UserService = Depends() , db: Session = Depends(get_db) ):
     try :
-        register_user = user_service.register_user(user, db)
+        register_user = AuthService.user_register(user , db)
         return ResponseHandler.success("create_success" ,register_user)
     except Exception as e:
         raise HTTPException(status_code=400, detail=str(e))
-# @router.get("/getUsers")
-# async def get_users(db: Session = Depends(get_db)):
-#     return UserService.getUsers(db)
 
-@router.post("/login" , response_model = DataResponse[Token])
-def login_access_token(data : login , service_user  : UserService = Depends() , db : Session = Depends(get_db) ):
-    user = service_user.authenticate(data,db)
+@router.post("/login" )
+def login_access_token(data : OAuth2PasswordRequestForm =Depends() , db : Session = Depends(get_db) ):
+    login_form = login(username=data.username, password=data.password)
+    user = AuthService.authenticate_user( login_form , db)
     if not user :
         raise (HTTPException(status_code=400, detail="Incorrect username or password"))
-    return DataResponse().success_response({
-        'access_token': create_access_token(user_id=user.user_id)
-    })
+    return {
+        'access_token': create_access_token(user_id=user.user_id) , "token_type": "bearer"
+    }
 
-@router.put("/profile" , dependencies = [Depends(login_required)], response_model = DataResponse[User])
-def update_profile(user_data : userUpdateRequest , current_user = Depends(current_user) ,user_service :UserService = Depends(), db : Session = Depends(get_db) ):
+@router.put("/profile" , dependencies = [Depends(login_required)])
+def update_profile(user_data : userUpdateRequest , current_user = Depends(AuthService.get_current_user) ,user_service :UserService = Depends(), db : Session = Depends(get_db) ):
     """"
     API update profile
     """
     try :
         update_profile = user_service.update_profile(data = user_data , current_user= current_user , db =  db )
-        return DataResponse().success_response(data = update_profile)
+        return ResponseHandler.success("update_profile" ,update_profile)
     except Exception as e:
         raise HTTPException(status_code=400, detail=str(e))
 
-
-
+@router.put("changePassword" , dependencies = [Depends(login_required)])
+def change_password(pass_word_form : passwordChangeRequest , current_user = Depends(AuthService.get_current_user), db : Session = Depends(get_db) ):
+    user =  UserService.change_password(pass_word_form,current_user, db)
+    return ResponseHandler.success("change_password_success" ,user)
+@router.get("/profile", dependencies = [Depends(login_required)])
+def get_profile( current_user = Depends(AuthService.get_current_user) , db : Session = Depends(get_db) ):
+    user = UserService.get_infor( current_user , db)
+    return ResponseHandler.success("user profile" ,user)
 
