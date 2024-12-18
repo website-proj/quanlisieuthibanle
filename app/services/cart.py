a2adacc37dae52bc34898db2f905ca465863228a
nguyencongmanh2004 <manh20102004@gmail.com>
2024-12-18 09:50:25 +0700

    update cart

--- app/services/cart.py
+++ app/services/cart.py
@@ -2,10 +2,11 @@
 
 from fastapi import Depends, HTTPException
 from sqlalchemy.orm import Session
+from starlette import status
 
 from app.db.base import get_db
 from app.model.Products_Categories import Product
-from app.schemas.schema_cart import CartItemCreate
+from app.schemas.schema_cart import CartItemCreate, CartItemUpdate
 from app.model.cart_model import CartItem, Cart
 from app.services.auth import AuthService
 
@@ -20,8 +21,14 @@ class CartService:
         db.refresh(cart)
         return cart
     @staticmethod
-    def add_product_to_cart(cart_item : CartItemCreate ,  db : Session):
-        exist_cart_item = db.query(CartItem).filter(CartItem.cart_id == cart_item.cart_id , CartItem.product_id == cart_item.product_id).first()
+    def add_product_to_cart(cart_item : CartItemCreate ,  db : Session , token : str = Depends(AuthService.oauth2_scheme)) :
+        current_user = AuthService.get_current_user(token , db)
+        exist_cart = db.query(Cart).filter(Cart.user_id == current_user.user_id).first()
+        if not exist_cart:
+            exist_cart = CartService.create_cart( token , db )
+        exist_cart_item= db.query(CartItem).filter(CartItem.cart_id == exist_cart.cart_id , CartItem.product_id == cart_item.product_id).first()
+        if not exist_cart_item:
+            raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail='CartItem not found')
         product = db.query(Product).filter(Product.product_id == cart_item.product_id).first()
         product_price = product.price * cart_item.quantity
         if exist_cart_item:
@@ -30,7 +37,7 @@ class CartService:
             db.commit()
             db.refresh(exist_cart_item)
             return exist_cart_item
-        cart_item = CartItem(cart_id = cart_item.cart_id, product_id = cart_item.product_id
+        cart_item = CartItem(cart_id = exist_cart.cart_id, product_id = cart_item.product_id
                              , quantity = cart_item.quantity , price_at_add = product_price  )
         db.add(cart_item)
         db.commit()
@@ -47,13 +54,17 @@ class CartService:
 
 
     @staticmethod
-    def update_product_quantity(cart_id : str , product_id : str  , quantity : int , db : Session):
-        cart_item = db.query(CartItem).filter(CartItem.cart_id == cart_id , CartItem.product_id == product_id).first()
-        product = db.query(Product).filter(Product.product_id == product_id).first()
-        price = product.price * quantity
+    def update_product_quantity(cart_update : CartItemUpdate, db : Session = Depends(get_db) , token : str = Depends(AuthService.oauth2_scheme) ) :
+        current_user = AuthService.get_current_user(token , db)
+        exist_cart = db.query(Cart).filter(Cart.user_id == current_user.user_id).first()
+        if not exist_cart:
+            raise HTTPException(status_code=404 , detail = "no cart found")
+        cart_item = db.query(CartItem).filter(CartItem.cart_id == exist_cart.cart_id , CartItem.product_id == cart_update.product_id).first()
+        product = db.query(Product).filter(Product.product_id == cart_update.product_id).first()
+        price = product.price * cart_update.quantity
         if not cart_item:
             raise HTTPException(status_code=404, detail="No products found")
-        cart_item.quantity = quantity
+        cart_item.quantity = cart_update.quantity
         cart_item.price_at_add = price
         db.commit()
         db.refresh(cart_item)
@@ -76,8 +87,12 @@ class CartService:
             db.commit()
         return cart_items
     @staticmethod
-    def total_price(cart_id : str , db: Session):
-        cart_items = db.query(CartItem).filter(CartItem.cart_id == cart_id).all()
+    def total_price( db: Session = Depends(get_db) , token : str = Depends(AuthService.oauth2_scheme)) :
+        current_user = AuthService.get_current_user(token , db)
+        cart = db.query(Cart).filter(Cart.user_id == current_user.user_id).first()
+        if not cart :
+            raise HTTPException(status_code=404 , detail = "no cart found")
+        cart_items = db.query(CartItem).filter(CartItem.cart_id == cart.cart_id ).all()
         if not cart_items:
             raise HTTPException(status_code = 404 , detail = "No cart_item found")
         total_price = 0
