91623b4a43208f86328ee4bd29e62b07898131d7
nguyencongmanh2004 <manh20102004@gmail.com>
2024-12-22 10:48:34 +0700

    add cart order and address

--- app/Routers/api_router.py
+++ app/Routers/api_router.py
@@ -1,6 +1,7 @@
 from fastapi import APIRouter
 
-from app.Routers import admin_user, admin_category, admin_product, categories, products, users, super_admin ,  Cart
+from app.Routers import admin_user, admin_category, admin_product, categories, products, users, super_admin, Cart, \
+    Order, Order_admin
 
 router = APIRouter()
 
@@ -11,3 +12,5 @@ router.include_router(products.router, tags=["Product"], prefix="/product")
 router.include_router(users.router, tags=["users"], prefix="/users")
 router.include_router(super_admin.router, tags=["super_admin"], prefix="/super_admin")
 router.include_router(Cart.router , tags=["Cart"], prefix="/cart")
+router.include_router(Order.router , tags=["Order"], prefix="/order")
+router.include_router(Order_admin.router, tags=["Order_admin"], prefix="/order_admin")
