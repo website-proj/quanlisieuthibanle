ad0376006fac268ab927487abe75a63ceb5930d1
nguyencongmanh2004 <manh20102004@gmail.com>
2025-01-03 10:51:25 +0700

    add more data

--- database/orderItem_add_data.sql
+++ database/orderItem_add_data.sql
@@ -1,33 +1,3 @@
-INSERT INTO Order_Items (order_id, product_id, quantity, price)
+-- Insert order items for orderb3c7ddfe01
+INSERT INTO order_item (order_item_id, order_id, product_id, quantity, price, total, date_created, updated_at)
 VALUES
-(1, 1, 3, 50000.00), -- Đơn hàng 1: 3 sản phẩm Mì Gói Hảo Hảo
-(1, 2, 2, 60000.00), -- Đơn hàng 1: 2 sản phẩm Nước Ngọt Coca-Cola
-(2, 3, 1, 40000.00), -- Đơn hàng 2: 1 sản phẩm Giò Chả Phú Hương
-(3, 4, 5, 8000.00),  -- Đơn hàng 3: 5 sản phẩm Bánh Gạo Lotte
-(4, 5, 3, 90000.00), -- Đơn hàng 4: 3 sản phẩm Nước Mắm Phú Quốc
-(5, 6, 2, 150000.00),-- Đơn hàng 5: 2 sản phẩm Bánh Quy Orion
-(6, 7, 4, 12000.00), -- Đơn hàng 6: 4 sản phẩm Gạo Nếp
-(7, 8, 6, 15000.00), -- Đơn hàng 7: 6 sản phẩm Nước Hoa Mùi Hương
-(8, 9, 1, 150000.00),-- Đơn hàng 8: 1 sản phẩm Sữa Tươi Vinamilk
-(9, 10, 2, 45000.00),-- Đơn hàng 9: 2 sản phẩm Trà Lipton
-(10, 11, 3, 10000.00),-- Đơn hàng 10: 3 sản phẩm Mì Gói Hảo Hảo
-(11, 12, 5, 20000.00),-- Đơn hàng 11: 5 sản phẩm Bánh Gạo Lotte
-(12, 13, 1, 40000.00),-- Đơn hàng 12: 1 sản phẩm Giò Chả Phú Hương
-(13, 14, 3, 120000.00),-- Đơn hàng 13: 3 sản phẩm Nước Mắm Phú Quốc
-(14, 15, 4, 35000.00),-- Đơn hàng 14: 4 sản phẩm Nước Ngọt Coca-Cola
-(15, 16, 2, 80000.00),-- Đơn hàng 15: 2 sản phẩm Bánh Quy Orion
-(16, 17, 5, 25000.00),-- Đơn hàng 16: 5 sản phẩm Gạo Nếp
-(17, 18, 1, 120000.00),-- Đơn hàng 17: 1 sản phẩm Sữa Tươi Vinamilk
-(18, 19, 2, 15000.00),-- Đơn hàng 18: 2 sản phẩm Trà Lipton
-(19, 20, 6, 30000.00),-- Đơn hàng 19: 6 sản phẩm Nước Hoa Mùi Hương
-(20, 21, 3, 70000.00),-- Đơn hàng 20: 3 sản phẩm Mì Gói Hảo Hảo
-(21, 22, 5, 10000.00),-- Đơn hàng 21: 5 sản phẩm Bánh Gạo Lotte
-(22, 23, 4, 45000.00),-- Đơn hàng 22: 4 sản phẩm Giò Chả Phú Hương
-(23, 24, 1, 180000.00),-- Đơn hàng 23: 1 sản phẩm Nước Mắm Phú Quốc
-(24, 25, 2, 13000.00),-- Đơn hàng 24: 2 sản phẩm Nước Ngọt Coca-Cola
-(25, 26, 3, 120000.00),-- Đơn hàng 25: 3 sản phẩm Bánh Quy Orion
-(26, 27, 6, 10000.00),-- Đơn hàng 26: 6 sản phẩm Gạo Nếp
-(27, 28, 2, 120000.00),-- Đơn hàng 27: 2 sản phẩm Sữa Tươi Vinamilk
-(28, 29, 5, 15000.00),-- Đơn hàng 28: 5 sản phẩm Trà Lipton
-(29, 30, 3, 22000.00),-- Đơn hàng 29: 3 sản phẩm Nước Hoa Mùi Hương
-(30, 1, 4, 50000.00);  -- Đơn hàng 30: 4 sản phẩm Mì Gói Hảo Hảo
