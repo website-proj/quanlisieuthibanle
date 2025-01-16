355f8cc798d6b979b9c7150b6687204b4b70315c
toi <calranguyen2702@gmail.com>
2025-01-17 03:15:40 +0700

    lazyloading tam

--- user/src/components/CardLoading/index.jsx
+++ user/src/components/CardLoading/index.jsx
@@ -1,13 +1,45 @@
-import React from "react";
+import React, { useRef, useEffect, useState } from "react";
 
 const CardLoading = () => {
+  const [isVisible, setIsVisible] = useState(false);
+  const ref = useRef(null);
+
+  useEffect(() => {
+    const observer = new IntersectionObserver(
+      (entries) => {
+        entries.forEach((entry) => {
+          if (entry.isIntersecting) {
+            setIsVisible(true); // Hiển thị khi phần tử trong viewport
+          }
+        });
+      },
+      { threshold: 0.1 } // Kích hoạt khi 10% của phần tử xuất hiện trong viewport
+    );
+
+    if (ref.current) {
+      observer.observe(ref.current);
+    }
+
+    return () => {
+      if (ref.current) {
+        observer.unobserve(ref.current);
+      }
+    };
+  }, []);
+
   return (
-    <div className="border py-2 lg:p-4 grid gap-1 lg:gap-3 min-w-36 lg:min-w-52 rounded cursor-pointer bg-white animate-pulse">
+    <div
+      ref={ref}
+      className={`border py-2 lg:p-4 grid gap-1 lg:gap-3 min-w-36 lg:min-w-52 rounded cursor-pointer bg-white animate-pulse transition-transform duration-700 ${
+        isVisible
+          ? "transform translate-x-0 opacity-100"
+          : "translate-x-full opacity-0"
+      }`}
+    >
       <div className="min-h-24 bg-blue-50 rounded"></div>
-      <div className="p-2 lg:p-3  bg-blue-50 rounded w-20"></div>
+      <div className="p-2 lg:p-3 bg-blue-50 rounded w-20"></div>
       <div className="p-2 lg:p-3 bg-blue-50 rounded"></div>
       <div className="p-2 lg:p-3 bg-blue-50 rounded w-14"></div>
-
       <div className="flex items-center justify-between gap-3">
         <div className="p-2 lg:p-3 bg-blue-50 rounded w-20"></div>
         <div className="p-2 lg:p-3 bg-blue-50 rounded w-20"></div>
--- user/src/components/Discounts/index.jsx
+++ user/src/components/Discounts/index.jsx
@@ -5,11 +5,13 @@ import { FiShoppingCart } from "react-icons/fi";
 import { CartContext } from "../../Context/CartContext"; // Import CartContext
 import axios from "axios";
 import SummaryApi, { baseURL } from "../../common/SummaryApi";
+import CardLoading from "../CardLoading";
 
 const DiscountProducts = () => {
   const [products, setProducts] = useState([]);
   const [visibleCount, setVisibleCount] = useState(10); // Số lượng sản phẩm hiển thị ban đầu
   const { incrementCartCount } = useContext(CartContext); // Lấy hàm tăng số lượng giỏ hàng từ context
+  const [isLoading, setIsLoading] = useState(true); // Thêm state isLoading
 
   useEffect(() => {
     const fetchDiscountProducts = async () => {
@@ -20,14 +22,16 @@ const DiscountProducts = () => {
         });
 
         if (response.data && Array.isArray(response.data.data)) {
-          setProducts(response.data.data); // Cập nhật sản phẩm vào state từ trường "data"
+          setProducts(response.data.data);
         } else {
           console.error("Dữ liệu không hợp lệ hoặc không có sản phẩm");
-          setProducts([]); // Gán mảng trống nếu dữ liệu không hợp lệ
+          setProducts([]);
         }
       } catch (error) {
         console.error("Error fetching discount products:", error);
-        setProducts([]); // Gán mảng trống khi có lỗi xảy ra
+        setProducts([]);
+      } finally {
+        setIsLoading(false); // Đặt isLoading thành false sau khi tải xong
       }
     };
 
@@ -133,49 +137,55 @@ const DiscountProducts = () => {
       </div>
 
       <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-5 gap-12 productListSale">
-        {products.slice(0, visibleCount).map((product) => (
-          <div
-            className="border flex flex-col justify-between shadow rounded-xl p-4 hover:shadow-lg transition-all duration-300 ease-in-out transform product_item"
-            key={product.product_id}
-          >
-            <Link to={`/product_detials/${product.product_id}`} state={product}>
-              <div className="relative overflow-hidden rounded-lg">
-                <img
-                  src={product.image}
-                  alt={product.name}
-                  className="w-full h-48 object-cover transition-transform duration-300 hover:scale-110"
-                />
+        {isLoading
+          ? Array(visibleCount)
+              .fill(0)
+              .map((_, index) => <CardLoading key={index} />)
+          : products.slice(0, visibleCount).map((product) => (
+              <div
+                className="border flex flex-col justify-between shadow rounded-xl p-4 hover:shadow-lg transition-all duration-300 ease-in-out transform product_item"
+                key={product.product_id}
+              >
+                <Link
+                  to={`/product_detials/${product.product_id}`}
+                  state={product}
+                >
+                  <div className="relative overflow-hidden rounded-lg">
+                    <img
+                      src={product.image}
+                      alt={product.name}
+                      className="w-full h-48 object-cover transition-transform duration-300 hover:scale-110"
+                    />
+                  </div>
+                  {product.discount && (
+                    <span className="absolute top-6 left-0 bg-[#1a73e8] text-white text-xs font-semibold px-2 py-1 rounded">
+                      {product.discount}%
+                    </span>
+                  )}
+                  <div className="mt-4">
+                    <h5 className="text-[0.9em] text-left">{product.name}</h5>
+                    <p className="text-[0.72em] text-black-500 text-left">
+                      ĐVT: {product.unit}
+                    </p>
+                    <div className="flex items-center justify-between mt-2 space-x-2">
+                      <span className="text-red-500 text-base font-bold">
+                        {formatCurrency(product.price)}
+                      </span>
+                      <span className="line-through text-sm text-gray-400">
+                        {formatCurrency(product.old_price)}
+                      </span>
+                    </div>
+                  </div>
+                </Link>
+                <Button
+                  onClick={(e) => handleAddToCart(e, product, 1)}
+                  className="productCart flex items-center whitespace-nowrap "
+                >
+                  <FiShoppingCart className="text-[2em] mt-auto pr-2" />
+                  Thêm vào giỏ hàng
+                </Button>
               </div>
-              {product.discount && (
-                <span className="absolute top-6 left-0 bg-[#1a73e8] text-white text-xs font-semibold px-2 py-1 rounded">
-                  {product.discount}%
-                </span>
-              )}
-              <div className="mt-4">
-                <h5 className="text-[0.9em] text-left">{product.name}</h5>
-                <p className="text-[0.72em] text-black-500 text-left">
-                  ĐVT: {product.unit}
-                </p>
-                <div className="flex items-center justify-between mt-2 space-x-2">
-                  <span className="text-red-500 text-base font-bold">
-                    {formatCurrency(product.price)}
-                  </span>
-                  <span className="line-through text-sm text-gray-400">
-                    {formatCurrency(product.old_price)}
-                  </span>
-                </div>
-              </div>
-            </Link>
-
-            <Button
-              onClick={(e) => handleAddToCart(e, product, 1)}
-              className="productCart flex items-center whitespace-nowrap "
-            >
-              <FiShoppingCart className="text-[2em] mt-auto pr-2" />
-              Thêm vào giỏ hàng
-            </Button>
-          </div>
-        ))}
+            ))}
       </div>
 
       {visibleCount < products.length && (
