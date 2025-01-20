c1b60eef2464aceaf4e8028a9ef22986f8b0b2d6
nguyenbathongg <nguyennbaathongg@gmail.com>
2025-01-20 23:23:37 +0700

    update

--- backend/app/model/voucher_payment.py
+++ backend/app/model/voucher_payment.py
@@ -6,7 +6,8 @@ from sqlalchemy.orm import relationship
 from app.model.model_base import Base
 class Voucher(Base):
     __tablename__ = 'vouchers'
-    voucher_id = Column(Integer, primary_key=True, default = lambda : f"voucher{uuid.uuid4()}")
+    voucher_id = Column(String(50), primary_key=True, unique=True, 
+                       default=lambda: f"voucher{uuid.uuid4().hex[:8]}")
     code = Column(String , unique=True , nullable=False)
     discount_rate = Column(Float, nullable=False)
     membership_required = Column(Enum("Silver", "Gold", "Diamond" , name = "membership_required"), nullable=False)
@@ -20,15 +21,18 @@ class Voucher(Base):
     payments = relationship("Payment", back_populates="voucher")
 class Payment(Base):
     __tablename__ = 'payments'
-    payment_id = Column(Integer, primary_key=True, nullable=False,default = lambda :f"payment{uuid.uuid4()}")
+    payment_id = Column(String(50), primary_key=True, unique=True,
+                       default=lambda: f"payment{uuid.uuid4().hex[:8]}")
     user_id = Column(String(50), ForeignKey('users.user_id'), nullable=False)
     order_id = Column(String(50), ForeignKey('orders.order_id'), nullable=False)
-    voucher_id = Column(String(50), ForeignKey('vouchers.voucher_id') , default = None)
+    voucher_id = Column(String(50), ForeignKey('vouchers.voucher_id'), default=None)
     amount = Column(Float, nullable=False)
-    payment_method = Column(Enum('Credit Card' , 'Debit Card' , 'Cash' , name = "payment method"), nullable=False)
-    payment_date = Column(DateTime, nullable=False , default = datetime.utcnow)
-    status = Column(Enum('Successful', 'Failed', 'Pending',name = "status") , nullable=False)
-    #relationship
+    payment_method = Column(Enum('Credit Card', 'Debit Card', 'Cash', name="payment_method"), 
+                          nullable=False)
+    payment_date = Column(DateTime, nullable=False, default=datetime.utcnow)
+    status = Column(Enum('Successful', 'Failed', 'Pending', name="status"), nullable=False)
+
+    # relationships
     user = relationship('User', back_populates='payments')
     orders = relationship('Orders', back_populates='payments')
     voucher = relationship('Voucher', back_populates='payments')
